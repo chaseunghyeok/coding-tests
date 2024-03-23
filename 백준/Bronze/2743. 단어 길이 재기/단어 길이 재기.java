f7cc672265d1322472371071dacd397653762915
hyeok <124764329+chaseunghyeok@users.noreply.github.com>
2024-03-23 11:57:04 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+
+    String S = scanner.next();
+
+    System.out.println(S.length());
+  }
+}
