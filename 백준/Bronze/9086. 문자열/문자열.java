10c6275cb30e1d69603bc90093acd60c009fd42b
hyeok <124764329+chaseunghyeok@users.noreply.github.com>
2024-03-24 22:05:31 +0900

    [Bronze V] Title: 문자열, Time: 212 ms, Memory: 17360 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+
+    for (int i = 0; i < N; i++) {
+      String S = scanner.next();
+      System.out.print(S.charAt(0));
+      System.out.println(S.charAt(S.length()-1));
+    }
+
+  }
+}
